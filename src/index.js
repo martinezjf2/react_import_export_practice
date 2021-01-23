426f77f87194e7c79503accd2f3505f0bd2a68b8
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-23 00:13:26 -0500

    exported all and imported all to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import * as Function from "./calculator";
 //Import the add, multiply, subtract and divide functions
 //from the calculator.js file.
 //If successful, your website should look the same as the Final.png
